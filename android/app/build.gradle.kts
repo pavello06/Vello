e41db4cf4ab1d1a2bb23a689e3930782aa8931e3
Pavel Halukha <pavelgaluha@gmail.com>
2025-05-16 09:25:44 +0300

    refactor: configure settings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,9 +9,9 @@ plugins {
 }
 
 android {
+    ndkVersion = "27.0.12077973"
     namespace = "com.example.vello"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.vello"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
