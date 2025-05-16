197451ed880b54b2c78e6ad38bc1f1a02662af13
Pavel Halukha <pavelgaluha@gmail.com>
2025-05-16 09:26:21 +0300

    feat: add dialogs for interaction

---
+++ lib/helper/dialogs.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Dialogs {
+  static void showSnackBar(BuildContext context, String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(msg),
+        backgroundColor: Colors.orange.withValues(alpha: 0.8),
+        behavior: SnackBarBehavior.floating,
+      ),
+    );
+  }
+
+  static void showProgressBar(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (_) => Center(child: const CircularProgressIndicator()),
+    );
+  }
+}
