2acc0a60aa409525c5a03fc6496e98e3e319d44e
Pavel Halukha <pavelgaluha@gmail.com>
2025-05-16 09:26:48 +0300

    feat: add sign in with google

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,6 +1,13 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:vello/main.dart';
-import 'package:vello/screens/home_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:vello/helper/dialogs.dart';
+
+import '../../main.dart';
+import '../home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -15,11 +22,52 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(microseconds: 500), () {
+    Future.delayed(const Duration(milliseconds: 500), () {
       setState(() {
         _isAnimate = true;
       });
     });
+  }
+
+  _handleGoogleBtnClick() {
+    Dialogs.showProgressBar(context);
+    _signInWithGoogle().then((user) {
+      Navigator.pop(context);
+
+      if (user != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => HomeScreen()),
+        );
+      }
+    });
+  }
+
+  Future<UserCredential?> _signInWithGoogle() async {
+    try {
+      await InternetAddress.lookup('google.com');
+
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } catch (err) {
+      log('\n_signInWithGoogle: $err');
+      Dialogs.showSnackBar(context, 'Something went wrong (check internet!)');
+      return null;
+    }
+  }
+
+  _signOut() async {
+    await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
   }
 
   @override
@@ -35,9 +83,9 @@ class _LoginScreenState extends State<LoginScreen> {
         children: [
           AnimatedPositioned(
             top: mq.height * 0.15,
-            left: _isAnimate ? mq.width * 0.25 : -mq.width * 0.5,
+            right: _isAnimate ? mq.width * 0.25 : -mq.width * 0.5,
             width: mq.width * 0.5,
-            duration: Duration(seconds: 1),
+            duration: Duration(milliseconds: 500),
             child: Image.asset('images/icon.png'),
           ),
           Positioned(
@@ -52,10 +100,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 elevation: 1,
               ),
               onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (_) => HomeScreen()),
-                );
+                _handleGoogleBtnClick();
               },
               icon: Image.asset('images/google.png', height: mq.height * 0.03),
               label: RichText(
