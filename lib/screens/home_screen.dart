488d040617be63aec81031826186cc3d71a9eca8
Pavel Halukha <pavelgaluha@gmail.com>
2025-05-16 20:43:12 +0300

    feat: add search

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/services.dart';
 import 'package:vello/models/chat_user.dart';
 import 'package:vello/screens/profile_screen.dart';
 import 'package:vello/widgets/chat_user_card.dart';
@@ -16,7 +16,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<ChatUser> list = [];
+  List<ChatUser> _list = [];
+
+  final List<ChatUser> _searchList = [];
+  bool _isSearching = false;
 
   @override
   void initState() {
@@ -26,61 +29,128 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(CupertinoIcons.home),
-        title: const Text('Vello'),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => ProfileScreen(user: APIs.me)),
-              );
-            },
-            icon: const Icon(Icons.more_vert),
-          ),
-        ],
-      ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 10),
-        child: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add_comment_rounded),
-        ),
-      ),
-      body: StreamBuilder(
-        stream: APIs.getAllUsers(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-            case ConnectionState.active:
-            case ConnectionState.done:
-              final data = snapshot.data?.docs;
-              list =
-                  data?.map((e) => ChatUser.fromJson(e.data())).toList() ?? [];
-
-              if (list.isNotEmpty) {
-                return ListView.builder(
-                  itemCount: list.length,
-                  padding: EdgeInsets.only(top: mq.height * 0.01),
-                  physics: const BouncingScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    return ChatUserCard(user: list[index]);
-                  },
-                );
-              } else {
-                return Center(
-                  child: Text(
-                    'No connections found!',
-                    style: TextStyle(fontSize: 20, color: Colors.orange),
-                  ),
-                );
-              }
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: PopScope<bool>(
+        canPop: false,
+        onPopInvokedWithResult: (bool didPop, bool? result) {
+          if (_isSearching) {
+            setState(() {
+              _isSearching = !_isSearching;
+            });
+            return;
           }
+          Future.delayed(
+            const Duration(milliseconds: 300),
+            SystemNavigator.pop,
+          );
         },
+        child: Scaffold(
+          appBar: AppBar(
+            leading: const Icon(CupertinoIcons.home),
+            title:
+                _isSearching
+                    ? TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Name, Email, ...',
+                      ),
+                      autofocus: true,
+                      style: TextStyle(
+                        fontSize: 17,
+                        letterSpacing: 0.5,
+                        color: Colors.white,
+                      ),
+                      onChanged: (value) {
+                        _searchList.clear();
+
+                        for (var i in _list) {
+                          if (i.name.toLowerCase().contains(
+                                value.toLowerCase(),
+                              ) ||
+                              i.email.toLowerCase().contains(
+                                value.toLowerCase(),
+                              )) {
+                            _searchList.add(i);
+                          }
+                        }
+                        setState(() {
+                          _searchList;
+                        });
+                      },
+                    )
+                    : const Text('Vello'),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _isSearching = !_isSearching;
+                  });
+                },
+                icon: Icon(
+                  _isSearching
+                      ? CupertinoIcons.clear_circled_solid
+                      : Icons.search,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => ProfileScreen(user: APIs.me),
+                    ),
+                  );
+                },
+                icon: const Icon(Icons.more_vert),
+              ),
+            ],
+          ),
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: FloatingActionButton(
+              onPressed: () {},
+              child: const Icon(Icons.add_comment_rounded),
+            ),
+          ),
+          body: StreamBuilder(
+            stream: APIs.getAllUsers(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                case ConnectionState.active:
+                case ConnectionState.done:
+                  final data = snapshot.data?.docs;
+                  _list =
+                      data?.map((e) => ChatUser.fromJson(e.data())).toList() ??
+                      [];
+
+                  if (_list.isNotEmpty) {
+                    return ListView.builder(
+                      itemCount:
+                          _isSearching ? _searchList.length : _list.length,
+                      padding: EdgeInsets.only(top: mq.height * 0.01),
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return ChatUserCard(
+                          user:
+                              _isSearching ? _searchList[index] : _list[index],
+                        );
+                      },
+                    );
+                  } else {
+                    return Center(
+                      child: Text(
+                        'No connections found!',
+                        style: TextStyle(fontSize: 20, color: Colors.orange),
+                      ),
+                    );
+                  }
+              }
+            },
+          ),
+        ),
       ),
     );
   }
