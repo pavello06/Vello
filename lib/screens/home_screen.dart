2f37984ca9d1efcc2ebc800c9b7a2693f21f2374
Pavel Halukha <pavelgaluha@gmail.com>
2025-05-16 16:25:59 +0300

    feat: draw list of chats

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,14 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:vello/models/chat_user.dart';
+import 'package:vello/widgets/chat_user_card.dart';
+
+import '../api/apis.dart';
+import '../main.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,11 +18,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<ChatUser> list = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(CupertinoIcons.home),
+        leading: const Icon(CupertinoIcons.home),
         title: const Text('Vello'),
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
@@ -23,9 +34,44 @@ class _HomeScreenState extends State<HomeScreen> {
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(bottom: 10),
         child: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () async {
+            await APIs.auth.signOut();
+            await GoogleSignIn().signOut();
+          },
           child: const Icon(Icons.add_comment_rounded),
         ),
+      ),
+      body: StreamBuilder(
+        stream: APIs.firestore.collection('users').snapshots(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+            case ConnectionState.done:
+              final data = snapshot.data?.docs;
+              list =
+                  data?.map((e) => ChatUser.fromJson(e.data())).toList() ?? [];
+
+              if (list.isNotEmpty) {
+                return ListView.builder(
+                  itemCount: list.length,
+                  padding: EdgeInsets.only(top: mq.height * 0.01),
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return ChatUserCard(user: list[index]);
+                  },
+                );
+              } else {
+                return Center(
+                  child: Text(
+                    'No connections found!',
+                    style: TextStyle(fontSize: 20, color: Colors.orange),
+                  ),
+                );
+              }
+          }
+        },
       ),
     );
   }
