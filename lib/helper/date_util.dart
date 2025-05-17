ec9dbb6e729a5588b422dd4b0a3c5ffa6942a624
Pavel Halukha <pavelgaluha@gmail.com>
2025-05-17 12:28:10 +0300

    feat: datetime converter from database

---
+++ lib/helper/date_util.dart
@@ -0,0 +1,13 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class DateUtil {
+  static String getFormattedTime({
+    required BuildContext context,
+    required String time,
+  }) {
+    final date = DateTime.fromMillisecondsSinceEpoch(int.parse(time));
+    return TimeOfDay.fromDateTime(date).format(context);
+  }
+}
